baaaf77ca556b30175e61f3d37d2f41a846e3da0
flavia santos <fs385953@gmail.com>
2025-04-07 23:29:04 -0300

    add novo arquivo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,31 +2,31 @@ let toggle = document.querySelector('.toggle');
 let navigation = document.querySelector('.navigation');
 let main = document.querySelector('.main')
 
-toggle.onclick = function(){
+toggle.onclick = function () {
     navigation.classList.toggle("active");
     main.classList.toggle("active")
 }
 
 
 
-async function user(){
-    const response = await fetch('https://doit-production.up.railway.app/person')
+async function user() {
+    const response = await fetch('https://doit-production.up.railway.app/person');
     return await response.json()
 }
 
-function getUserprofile(){
+function getUserprofile() {
     user().then(userData => {
         let userInfo = ""
         userData.forEach(item => {
-           userInfo += `<div class="data">
-                     <img src="avatar" alt="foto do perfil">
+            userInfo += `<div class="data">
+                            <img src="" alt="foto do perfil"/>
                                <h1>${item.name}</h1>
                                <p>${item.age}</p>
                             </div>`
-                        })
-    
+        })
+
         document.querySelector('.profile-data').innerHTML = userInfo
-        
+
     })
 }
 
